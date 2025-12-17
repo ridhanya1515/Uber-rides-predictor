61501397fa05ba1606129897cbca4652284a3e7d
ridhanya1515 <ridhustar15@gmail.com>
2025-12-17 14:32:12 +0530

    Refactor model loading and format code
    Refactor model loading and improve code formatting.

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import pickle
 from flask import Flask, request, render_template
 import numpy as np
 import math
-import os   
+import os
 
 app = Flask(__name__)
-model2 = pickle.load(open('model.pkl', 'rb'))
+
+model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+model2 = pickle.load(open(model_path, 'rb'))
 
 @app.route('/')
 def home():
@@ -13,11 +15,14 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_feature = [int(i) for i in request.form.values()]  # [1,2,3,5]
+    int_feature = [int(i) for i in request.form.values()]
     final_feature = np.array(int_feature).reshape(1, -1)
     prediction = model2.predict(final_feature)
     output = round(prediction[0], 2)
-    return render_template('index.html', predict_text=f'Number of weekly rides {math.floor(output)}')
+    return render_template(
+        'index.html',
+        predict_text=f'Number of weekly rides {math.floor(output)}'
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
